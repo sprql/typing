97d49b9d576386c529732b07f274d2a7d3514638
Alexander Obukhov <github@sprql.space>
2018-01-11 22:00:16 +0300

    Add COLEMAK layout

--- javascripts/typing/exercises.js
+++ javascripts/typing/exercises.js
@@ -1,4 +1,5 @@
 Typing.exercises = {
+    'colemak' : [].concat(Typing.lessons.enbooks, Typing.lessons.special),
     'dvorak' : Typing.lessons.dvorak.concat(Typing.lessons.enbooks, Typing.lessons.special),
     'qwerty' : Typing.lessons.qwerty.concat(Typing.lessons.enbooks, Typing.lessons.special),
     'russian' : Typing.lessons.russian.concat(Typing.lessons.rubooks)
